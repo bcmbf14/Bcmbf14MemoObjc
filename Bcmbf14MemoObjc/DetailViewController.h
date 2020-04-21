9707cf2184134d9092883d390973f9340f42a55f
bcmbf14 <bcmbf14@gmail.com>
2020-04-22 06:10:57 +0900

    step18

---
+++ Bcmbf14MemoObjc/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Bcmbf14MemoObjc
+//
+//  Created by jongchankim on 2020/04/22.
+//  Copyright © 2020 jongchankim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
