858d60fb16cb9062e432835e556bd2de8df6105d
bcmbf14 <bcmbf14@gmail.com>
2020-04-23 07:39:05 +0900

    step36

--- Bcmbf14MemoObjc/ComposeViewController.m
+++ Bcmbf14MemoObjc/ComposeViewController.m
@@ -19,16 +19,36 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *memoTextView;
 
-
-
-
-
+@property (strong, nonatomic) id willShowToken;
+@property (strong, nonatomic) id willHideToken;
 
 
 
 @end
 
 @implementation ComposeViewController
+
+- (void) dealloc {
+    if(self.willShowToken) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self.willShowToken];
+    }
+    
+    if(self.willHideToken) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self.willHideToken];
+    }
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self.memoTextView becomeFirstResponder];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    
+    [self.memoTextView resignFirstResponder];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -41,17 +61,46 @@
         self.navigationItem.title = @"새 메모";
         self.memoTextView.text = @"";
     }
+    
+    
+    self.willShowToken = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillShowNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        
+        CGFloat height = [note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+        
+        UIEdgeInsets inset = self.memoTextView.contentInset;
+        inset.bottom = height;
+        self.memoTextView.contentInset = inset;
+        
+        inset = self.memoTextView.scrollIndicatorInsets;
+        inset.bottom = height;
+        self.memoTextView.scrollIndicatorInsets = inset;
+        
+    }];
+    
+    
+    
+    self.willHideToken = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillHideNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        
+        UIEdgeInsets inset = self.memoTextView.contentInset;
+        inset.bottom = 0;
+        self.memoTextView.contentInset = inset;
+        
+        inset = self.memoTextView.scrollIndicatorInsets;
+        inset.bottom = 0;
+        self.memoTextView.scrollIndicatorInsets = inset;
+    }];
+    
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 
 - (IBAction)close:(id)sender {
